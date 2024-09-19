0bfa8595bc68ebf9fb1e8fb230300f093ba99ea6
unknown <htp898@uowmail.edu.au>
2024-09-19 19:30:29 +1000

    chore: Clean eda_pp in a few tasks
    - These tasks were copied from clustering

--- regression.py
+++ regression.py
@@ -8,7 +8,9 @@ warnings.filterwarnings('ignore')
 
 REQS = [
     ('pip', 'pip==24.2'),
+    ('lightgbm', 'lightgbm==4.5.0'),
     ('matplotlib', 'matplotlib==3.9.2'),
+    ('mlxtend', 'mlxtend==0.23.1'),
     ('nltk', 'nltk==3.9.1'),
     ('numpy', 'numpy==2.0.2'),
     ('optuna', 'optuna==4.0.0'),
@@ -16,7 +18,8 @@ REQS = [
     ('seaborn', 'seaborn==0.13.2'),
     ('sklearn', 'scikit-learn==1.5.2'),
     ('statsmodels', 'statsmodels==0.14.3'),
-    ('umap-learn', 'umap-learn==0.5.6')
+    ('umap-learn', 'umap-learn==0.5.6'),
+    ('xgboost', 'xgboost==2.1.1'),
 ]
 
 try:
@@ -49,12 +52,19 @@ from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 
 # Machine learning and data processing
-from sklearn.preprocessing import StandardScaler
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans, DBSCAN
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import (
+    accuracy_score,
+    calinski_harabasz_score,
+    classification_report,
+    confusion_matrix,
+    mean_squared_error,
+    silhouette_score
+)
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import silhouette_score, calinski_harabasz_score, mean_squared_error
+from sklearn.preprocessing import StandardScaler
 
 # Statistical modeling
 import statsmodels.api as sm
@@ -63,14 +73,20 @@ from statsmodels.tools.tools import add_constant
 # Natural Language Processing
 import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 
 # Dimensionality reduction
 import umap
 
 # Hyperparameter optimization
 import optuna
+
+# Other machine learning libraries
+import lightgbm as lgb
+from xgboost import XGBClassifier
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
 
 
 def find_columns_with_missing(data, columns):
@@ -138,41 +154,7 @@ df_cleaned = df_cleaned.dropna(subset=['gender'])
 # Drop the 'profile_yn' column since it is not relevant to human/non-human classification
 df_cleaned = df_cleaned.drop(columns=['profile_yn'])
 
-# Now that we have handled the missing data, you can proceed with further analysis
-# print('The information of the cleaned dataset')
-# print(df_cleaned.info())
-# print('The first few rows of the cleaned dataset')
-# print(df_cleaned.head())
-
 current_num_features = df.select_dtypes(include=[np.number])
-
-# Plot distribution of each numerical feature with gender as hue using seaborn
-# for feature in current_num_features:
-    # plt.figure(figsize=(8, 6))
-    # sns.histplot(df_cleaned, x=feature, hue='gender', bins=30, kde=True)
-    # plt.title(f'Distribution of {feature} by Gender')
-    # plt.show()
-
-# Distribution of gender
-# plt.figure(figsize=(8, 6))
-# sns.countplot(x='gender', data=df_cleaned)
-# plt.title('Distribution of Gender')
-# plt.xlabel('Gender')
-# plt.ylabel('count')
-# plt.show()
-
-# Plot distribution of 'tweet_count' and 'retweet_count'
-# for column in ['tweet_count', 'retweet_count']:
-    # plt.figure(figsize=(8, 6))
-    # sns.histplot(data=df_cleaned, x=column, kde=True, bins=30)
-    # plt.title(f'Distribution of {column.replace("_", " ").capitalize()}')
-    # plt.show()
-
-# Correlation analysis for numerical features
-# plt.figure(figsize=(10, 8))
-# sns.heatmap(df_cleaned[['tweet_count', 'retweet_count', 'fav_number']].corr(), annot=True, cmap='coolwarm', vmin=-1, vmax=1)
-# plt.title('Correlation Matrix of Numerical Features')
-# plt.show()
 
 # Extracting date from 'created' and 'tweet_created' for time-based analysis
 df_cleaned['profile_created_year'] = pd.to_datetime(df_cleaned['created']).dt.year
@@ -189,44 +171,15 @@ df_cleaned['tweets_per_day'] = df_cleaned['tweet_count'] / df_cleaned['account_a
 df_cleaned['retweets_per_day'] = df_cleaned['retweet_count'] / df_cleaned['account_age']
 df_cleaned['favorites_per_day'] = df_cleaned['fav_number'] / df_cleaned['account_age']
 
-# Plotting the distribution of profile creation over the years
-# plt.figure(figsize=(8, 6))
-# sns.histplot(df_cleaned['profile_created_year'], kde=False, bins=15)
-# plt.title('Distribution of Profile Creation Years')
-# plt.xlabel('Profile Created Year')
-# plt.ylabel('count')
-# plt.show()
-
-# Plotting the histogram of tweets per day
-# plt.figure(figsize=(10, 6))
-# sns.histplot(df_cleaned['tweets_per_day'], bins=50, kde=True)
-# plt.title('Distribution of Tweets Per Day')
-# plt.xlabel('Tweets Per Day')
-# plt.ylabel('Frequency')
-# plt.show()
-
-# show the relationship between account age and tweets per day
-# plt.figure(figsize=(10, 6))
-# sns.scatterplot(x='account_age', y='tweets_per_day', data=df_cleaned)
-# plt.title('Account Age vs. Tweets Per Day')
-# plt.xlabel('Account Age (Days)')
-# plt.ylabel('Tweets Per Day')
-# plt.show()
-
 # Exploring 'link_color' and 'sidebar_color' features
 
 # Check number of NaN value in  'link_color' and 'sidebar_color' features
 link_color_nan_count = df_cleaned['link_color'].isnull().sum()
 sidebar_color_nan_count = df_cleaned['sidebar_color'].isnull().sum()
 
-# print(f"Number of NaN values in 'link_color': {link_color_nan_count}")
-# print(f"Number of NaN values in 'sidebar_color': {sidebar_color_nan_count}")
-
 # Check how many available colors in 'link_color' and 'sidebar_color' features
 link_color_count = len(df_cleaned['link_color'].unique())
 sidebar_color_count = len(df_cleaned['sidebar_color'].unique())
-# print(f'the number of link color is {link_color_count}')
-# print(f'the number of side bar color is {sidebar_color_count}')
 
 # Apply the function to 'link_color' and 'sidebar_color'
 df_cleaned['link_color'] = df_cleaned['link_color'].apply(lambda x: f'#{x}' if len(x) == 6 else '#000000')
@@ -235,70 +188,10 @@ df_cleaned['sidebar_color'] = df_cleaned['sidebar_color'].apply(lambda x: f'#{x}
 # Drop rows where 'sidebar_color' is still NaN
 df_cleaned = df_cleaned.dropna(subset=['link_color'])
 df_cleaned = df_cleaned.dropna(subset=['sidebar_color'])
-# print(f"Number of NaN values in 'link_color': {df_cleaned['link_color'].isnull().sum()}")
-# print(f"Number of NaN values in 'sidebar_color': {df_cleaned['sidebar_color'].isnull().sum()}")
 
 # top 15 colors
 top_sidebar_colors = df_cleaned['sidebar_color'].value_counts().iloc[:15].index.tolist()
 top_link_colors = df_cleaned['link_color'].value_counts().iloc[:15].index.tolist()
-# print(top_sidebar_colors)
-
-# Extract top 10 most common sidebar colors 
-# sns.set(rc={'axes.facecolor':'lightgrey', 'figure.facecolor':'white'})
-# plt.figure(figsize=(8, 6))
-# sns.countplot(y='sidebar_color', data=df_cleaned, order=df_cleaned['sidebar_color'].value_counts().iloc[:15].index, palette=top_sidebar_colors)
-# plt.title('Top 15 Most Common Profile sidebar_color')
-# plt.ylabel('Sidebar Color')
-# plt.xlabel('count')
-# plt.grid()
-# plt.show()
-
-# Extract top 10 most common link colors 
-# sns.set(rc={'axes.facecolor':'lightgrey', 'figure.facecolor':'white'})
-# plt.figure(figsize=(8, 6))
-# sns.countplot(y='link_color', data=df_cleaned, order=df_cleaned['link_color'].value_counts().iloc[:15].index, palette=top_link_colors)
-# plt.title('Top 15 Most Common Profile link_color')
-# plt.ylabel('Link Color')
-# plt.xlabel('count')
-# plt.grid()
-# plt.show()
-
-# count plot for sidebar_color vs. gender
-# plt.figure(figsize=(10, 6))
-# sns.set(rc={'axes.facecolor':'white', 'figure.facecolor':'white'})
-# sns.countplot(x='sidebar_color', hue='gender', data=df_cleaned, order=df_cleaned['sidebar_color'].value_counts().iloc[:15].index)
-# plt.title('Top 15 Most Common Sidebar Colors by Gender')
-# plt.xlabel('Sidebar Color')
-# plt.ylabel('count')
-# plt.xticks(rotation=45)
-# plt.show()
-
-# count plot for link_color vs. gender
-# plt.figure(figsize=(10, 6))
-# sns.countplot(x='link_color', hue='gender', data=df_cleaned, order=df_cleaned['link_color'].value_counts().iloc[:15].index)
-# plt.title('Top 15 Most Common link Colors by Gender')
-# plt.xlabel('Link Color')
-# plt.ylabel('count')
-# plt.xticks(rotation=45)
-# plt.show()
-
-# Scatter plot for link_color vs. tweet_count with gender as hue
-# plt.figure(figsize=(10, 6))
-# sns.scatterplot(x='link_color', y='tweet_count', hue='gender', data=df_cleaned[df_cleaned['link_color'].isin(top_link_colors)], palette='Set2', s=100, alpha=0.7)
-# plt.title('Link Colors vs. Tweet count with Gender')
-# plt.xlabel('Link Color')
-# plt.ylabel('Tweet count')
-# plt.xticks(rotation=45)
-# plt.show()
-
-# Scatter plot for sidebar_color vs. tweet_count with gender as hue
-# plt.figure(figsize=(10, 6))
-# sns.scatterplot(x='sidebar_color', y='tweet_count', hue='gender', data=df_cleaned[df_cleaned['sidebar_color'].isin(top_sidebar_colors)], palette='Set2', s=100, alpha=0.7)
-# plt.title('Sidebar Colors vs. Tweet count with Gender')
-# plt.xlabel('Sidebar Color')
-# plt.ylabel('Tweet count')
-# plt.xticks(rotation=45)
-# plt.show()
 
 # Select columns to be used
 col = ['gender', 'gender:confidence', 'description', 'favorites_per_day','link_color',
@@ -308,12 +201,8 @@ df_preprocessed = df_cleaned[col].copy()
 # Remove rows where gender is 'Unknown'
 df_preprocessed = df_preprocessed[df_preprocessed['gender'] != 'unknown']
 
-# Plot correlation matrix
-corr_matrix = df_preprocessed.select_dtypes(include=[np.number]).corr()
-# sns.heatmap(corr_matrix, annot=True)
-# plt.show()
-
 # Drop one feature from highly correlated pairs (correlation > 0.9)
+corr_matrix = df_preprocessed.select_dtypes(include=[np.number]).corr()
 upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
 to_drop = [column for column in upper.columns if any(upper[column] > 0.9)]
 df_preprocessed = df_preprocessed.drop(columns=to_drop)
@@ -339,18 +228,6 @@ df_preprocessed['user_timezone_encoded'] = df_preprocessed['user_timezone'].map(
 # encode the 'gender' column to numeric values
 df_preprocessed['gender'] = df_preprocessed['gender'].replace({'male': 0, 'female': 1, 'brand': 2})
 
-# Check for unique values in the 'gender' column after replacement
-# print(df_preprocessed['gender'].unique())
-# print(df_preprocessed.info())
-
-# Distribution of gender
-# plt.figure(figsize=(8, 6))
-# sns.countplot(x='gender', data=df_preprocessed)
-# plt.title('Distribution of Gender')
-# plt.xlabel('Gender')
-# plt.ylabel('count')
-# plt.show()
-
 df_gender = df_preprocessed[['gender', 'gender:confidence']].copy()
 
 # Drop the original categorical columns
@@ -367,22 +244,18 @@ rgb_df = pd.concat([rgb_df, pd.DataFrame(df_preprocessed['sidebar_color_rgb'].to
 # Drop the original color features
 df_preprocessed = df_preprocessed.drop(columns=['link_color', 'sidebar_color', 'link_color_rgb', 'sidebar_color_rgb'])
 
-# Check if all required features are there
-# print(f'All features that will be used are {df_preprocessed.columns.tolist()}')
-
 # Define the numerical features to scale (filtering for int64 and float64 columns)
 numerical_features = df_preprocessed.select_dtypes(include=[np.number])
-# print(f'All current numerical features are {numerical_features.columns.tolist()}')
 
-# print('After all, here is the information of the dataset')
-# print(df_preprocessed.info())
+print()
+print('---- NLP Processing ----')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('wordnet')
 
 df_preprocessed['description'].fillna('', inplace=True)
 df_preprocessed['text'].fillna('', inplace=True)
-# df_preprocessed['name'].fillna('', inplace=True)
-
-# Check the text features if they still contain NaN
-# print(df_preprocessed.select_dtypes(include=[object]))
 
 
 # Define stopwords and lemmatizer
@@ -392,10 +265,6 @@ lemmatizer = WordNetLemmatizer()
 # Apply preprocessing to the 'description', 'text', and 'name' columns
 df_preprocessed['cleaned_description'] = df_preprocessed['description'].apply(lambda x: preprocess_text(str(x)))
 df_preprocessed['cleaned_text'] = df_preprocessed['text'].apply(lambda x: preprocess_text(str(x)))
-# df_preprocessed['cleaned_name'] = df_preprocessed['name'].apply(lambda x: preprocess_text(str(x)))
-
-# Check the preprocessed data with preprocessed text features
-# print(df_preprocessed[['description', 'cleaned_description', 'text', 'cleaned_text']].head())
 
 # Drop the original text features
 df_preprocessed = df_preprocessed.drop(columns=['description','text'])
@@ -407,12 +276,10 @@ tfidf_vectorizer = TfidfVectorizer(max_features=1500, stop_words='english')
 
 tfidf_description = tfidf_vectorizer.fit_transform(df_preprocessed['cleaned_description']).toarray()
 tfidf_text = tfidf_vectorizer.fit_transform(df_preprocessed['cleaned_text']).toarray()
-# tfidf_name = tfidf_vectorizer.fit_transform(df_preprocessed['cleaned_name']).toarray()
 
 # Convert TF-IDF into DataFrames and add to df_preprocessed
 tfidf_desc_df = pd.DataFrame(tfidf_description, columns=[f'desc_{i}' for i in range(tfidf_description.shape[1])])
 tfidf_text_df = pd.DataFrame(tfidf_text, columns=[f'text_{i}' for i in range(tfidf_text.shape[1])])
-# tfidf_name_df = pd.DataFrame(tfidf_name, columns=[f'name_{i}' for i in range(tfidf_name.shape[1])])
 
 # Merge with main dataframe
 df_preprocessed = pd.concat([df_preprocessed.reset_index(drop=True), tfidf_desc_df, tfidf_text_df], axis=1)
@@ -422,8 +289,7 @@ df_preprocessed = df_preprocessed.drop(columns=['cleaned_description', 'cleaned_
 
 df_preprocessed = pd.concat([df_preprocessed, rgb_df], axis=1)
 
-# ================================================REGRSSION CODE============================
-# Complete py code ¨
+# ============================== REGRESSION ==============================
 
 # finish preprocessing for regression
 df_preprocessed_reg = df_preprocessed.copy()
